7eac4279652c8ddbc456f8166c6ad968d80d9559
Kyle Martin <kylecat98@gmail.com>
2017-07-24 20:49:24 -0400

    Fixed Growing Algorithm

--- Plugins/datTest/datTestTypes.cpp
+++ Plugins/datTest/datTestTypes.cpp
@@ -17,13 +17,26 @@ void Flower::update(unsigned int resolution)
 {
   age += resolution;
 
-  double gexChance = ((germinationMaxAge-germinationMinAge)-(germinationMaxAge-age)) / (germinationMaxAge-germinationMinAge);
+  auto gexChance = [=](int age)->double { return double(age-germinationMinAge) / double(germinationMaxAge-germinationMinAge);};
+  auto distribution = [=](int age)->double { return pow( 2.0*(-0.1*gexChance(age) / (gexChance(age) - 1.0)), 3.0 ); };
 
-  if (size <= maxSize)
-    if ((age <= germinationMaxAge) && (age >= germinationMinAge))
-      if (chance < pow(((-0.1*gexChance) / (gexChance - 1.0)), 4.0))
-        size += 0.1;
+  if (size < maxSize) //If I can grow continue
+  {
+    //If you're jumping over the grow period due to resolution
+    if ((age-resolution <= germinationMinAge) && (age >= germinationMaxAge))
+      size = maxSize;
+
+    //If my grow start time has passed
+    else if (chance < distribution(age))
+    {
+      //If this is the first time past my grow time
+      if ( !(chance < distribution(age-resolution)) )
+        size += (maxSize/(60.0*60.0*12.0))*(( chance-distribution(age) ) * (germinationMaxAge-germinationMinAge)); //How much the chance was missed by in world seconds
+      else //If nothing else has happened - everything is normal
+        size += (maxSize/(60.0*60.0*12.0))*resolution; //Grows to full size in 12 hours
+    }
   }
+}
 
 double Flower::getSize() const
 {
